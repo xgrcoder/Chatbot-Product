8390f223d393dbeb6d67975cc2a14c62176f8ff0
Abdul Aziz <abdulworkspace@outlook.com>
2026-04-08 15:46:09 +0100

    feat: embed Zempotis chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        {/* Zempotis Chat widget */}
+        <script src="https://zempotis-chat-fy989x4cu-abdul-aziz-uddins-projects.vercel.app/widget.js" data-client="organic-trust" async></script>
+      </body>
     </html>
   );
 }
